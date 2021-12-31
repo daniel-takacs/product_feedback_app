2fe0f58c0e28c7b51ee47fcf1c2e754bd169f8b0
daniel-takacs <takacsd89@gmail.com>
2021-12-31 21:59:59 +0200

    create feedback page design

--- client/src/style/Buttons.jsx
+++ client/src/style/Buttons.jsx
@@ -7,4 +7,19 @@ export const Button1 = styled.button `
     border: none;
     padding: 15px 25px;
     border-radius: 10px;
+    font-weight: bold;
+`
+export const CancelButton = styled.button `
+    background-color: #3A4374;
+    padding: 15px 25px;
+    border: none;
+    color: white;
+    border-radius: 10px;
+    font-weight: bold;
+`
+export const GoBackButton = styled.button `
+    background-color: transparent;
+    border: none;
+    font-weight: bold;
+    color: #647196;
 `
